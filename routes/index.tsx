e4357449adff049c18f0f40bcb61353b70411374
Abhishek Rawat <abhi.rawat456@gmail.com>
2024-04-26 19:01:39 +0530

    feat: load data from lambda

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,42 @@
-import { useSignal } from '@preact/signals'
-import Counter from '../islands/Counter.tsx'
+import { array, boolean, object, parse, string } from 'valibot'
 
-export default function Home() {
-	const count = useSignal(3)
+const ProblemSchema = object({
+	result: array(object({
+		'difficulty': string(),
+		'free': boolean(),
+		'id': string(),
+		'name': string(),
+		'problem_link': string(),
+		'solution_walkthrough': string(),
+		'group': string(),
+	})),
+})
+
+export default async function Home() {
+	const response = await fetch('https://nfjx7ug3qec76hujjep5wyjeya0bvovn.lambda-url.ap-south-1.on.aws/')
+	const data = await response.json()
+
+	const parsedData = parse(ProblemSchema, data)
+
 	return (
-		<div class='px-4 py-8 mx-auto bg-[#86efac]'>
+		<div class='px-4 py-8'>
 			<div class='max-w-screen-md mx-auto flex flex-col items-center justify-center'>
-				<img
-					class='my-6'
-					src='/logo.svg'
-					width='128'
-					height='128'
-					alt='the Fresh logo: a sliced lemon dripping with juice'
-				/>
-				<h1 class='text-4xl font-bold'>Welcome to Fresh</h1>
-				<p class='my-4'>
-					Try updating this message in the
-					<code class='mx-2'>./routes/index.tsx</code> file, and refresh.
-				</p>
-				<Counter count={count} />
+				<h1 class='text-4xl font-bold'>Welcome to Blind75</h1>
+				<h2 class='text-left font-medium my-2'>Total Problems: {parsedData.result.length}</h2>
+
+				<ul class='space-y-2 mt-4 list-decimal'>
+					{parsedData.result.map((problem) => (
+						<li>
+							<a href={problem.problem_link} target='_blank' rel='noreferrer'>{problem.name}</a>
+							<div>
+								<span>Difficulty: {problem.difficulty}</span>
+								<a class='block w-max' href={problem.solution_walkthrough} target='_blank' rel='noreferrer'>
+									Solution Walkthrough
+								</a>
+							</div>
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	)
